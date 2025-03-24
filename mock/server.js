7a9b201fcff582997bba7a50f4192a5a552bd603
Yuvaraj Nithyanandan <yuvarajn25@gmail.com>
2025-03-24 22:54:51 +0530

    cors origin fix on mock server

--- mock/server.js
+++ mock/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // const cors = require("cors");
 // if needed to be reached from another localhost server for local testing a distributed app f.e.
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header(
         "Access-Control-Allow-Headers",
